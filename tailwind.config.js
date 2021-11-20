af720b0df52fb68e1105ed5ea993a81d07241230
omar-faruk02 <tuhin.omar99@gmail.com>
2021-11-20 12:48:00 +0600

    git './pages//*.{js,ts,jsx,tsx}', './components//*.{js,ts,jsx,tsx}'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: [false],
+  purge: [
+    './pages//*.{js,ts,jsx,tsx}', './components//*.{js,ts,jsx,tsx}'
+  ],
   darkMode: false, // or 'media' or 'class'
   
   variants: {

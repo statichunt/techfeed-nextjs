4cced4530ddfbca90a7f1ebd2747e2bd80eb62d5
omar-faruk02 <tuhin.omar99@gmail.com>
2021-11-20 13:38:53 +0600

    commit

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Author from '../component/Author'
 import Post from '../component/Post'
 const matter=require('gray-matter')
 const path = require('path');
+
 const Home=({posts,page})=> {
  const post = posts.sort((a,b)=> new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
 
@@ -23,18 +24,20 @@ const Home=({posts,page})=> {
 export const getServerSideProps=async ({query:{page=1}})=>{
 
    
-    const files = fs.readdirSync(path.join('posts'))
+    // const files = fs.readdirSync(path.join('posts'))
     
-   
+    const directoryPath = path.join(process.cwd(), "posts");
+    const files = fs.readdirSync(directoryPath);
+  
   // const files= fs.readdirSync(PostPath)
   // const filesz= fs.readdirSync(path.join('About'))
   // console.log(filesz)
   
   const posts=files.map((filename)=>{
     const slug=filename.replace('.md','')
-    // const postFilePath=join(PostPath,filename)
-    // const metaDataWithFrontMatter= fs.readFileSync(postFilePath,'utf-8') 
-    const metaDataWithFrontMatter = fs.readFileSync(path.join('posts', filename), 'utf-8')
+    const postFilePath=path.join(directoryPath,filename)
+    const metaDataWithFrontMatter= fs.readFileSync(postFilePath,'utf-8') 
+    // const metaDataWithFrontMatter = fs.readFileSync(path.join('posts', filename), 'utf-8')
     const {data:frontmatter,content}=matter(metaDataWithFrontMatter)
     return{
       slug,

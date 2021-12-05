965352df9c5f193c5f1a33af6b126da566d27004
omar-faruk02 <tuhin.omar99@gmail.com>
2021-12-05 17:03:36 +0600

    mail

--- component/ContactPage/Contact.js
+++ component/ContactPage/Contact.js
@@ -1,38 +1,32 @@
-import React from 'react'
-import emailjs from 'emailjs-com';
+
+import { data } from "autoprefixer";
+import { useForm } from "react-hook-form";
+
 
 const Contact = () => {
-    const sendEmail = (e) => {
-        e.preventDefault();
-    
-        emailjs.sendForm('service_jvecemf', 'template_9x41wsk',e.target, 'user_9zzT5dxjTBAiznsMfcOTN')
-        
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-          e.target.reset()
-      };
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = (data,e) => {
+        fetch("http://localhost:3000/api/mail",{
+            method:'POST',
+            header:{'content-type':'application/json'},
+            body:JSON.stringify(data)
+        })
+        console.log(data)
+        e.target.reset()
+    } 
+  
+  
     return (
-        <>
-        <div className="md:w-4/5 px-10 w-full mx-auto">
-               <h1 className="heading text-center">Get In Touch</h1>
-               
-        <form className="mx-auto" onSubmit={sendEmail}>
-              <div className="">
-                  <div className="w-1/2"><input type="text" className="inputField" placeholder="First Name" name="First Name" /></div>
-                  <div className="w-1/2"><input type="text" className="inputField" placeholder="Last Name" name="Last Name" /></div>
-                  <div className="w-1/2"><input type="email" className="inputField" placeholder="First Name" name="email" /></div>
-                  <div className="w-1/2"><input type="text" className="inputField" placeholder="subject" name="subject" /></div>
-                  <div className="w-1/2"><input type="file" className="inputField" name="file" /></div>
-                  <textarea className="inputField" type="text" rows="7" name="message"></textarea>
-                  <button type="submit" className="block p-2 bg-yellow-500 rounded-full">Submit</button>
-              </div>
-        </form> 
-        </div> 
-        </>
-    )
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input className="block p-3 border-2"  {...register("name")} />
+    
+        <input  className="block p-3 border-2"  {...register("email", { required: true })} />
+        {errors.exampleRequired && <span>This field is required</span>}
+        <textarea className="block border-2" {...register("message", { required: true })}></textarea>
+        
+        <input type="submit" />
+      </form>
+    );
 }
 
 export default Contact
---
+++ pages/api/mail.js
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import nodemailer from 'nodemailer'
+
+export default async function handler(req, res) {
+  console.log(req.body)
+  const {name,email,message}=req.body
+ const transporter= nodemailer.createTransport({
+    pool: true,
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // use TLS
+    auth: {
+      user: 'faruk.themefisher@gmail.com',
+      pass: 'czjprbvbxuazgnkw',
+    },
+  });
+  try {
+    const emailTransport= await transporter.sendMail({
+      from:email,
+      to:"rakibur74@gmail.com",
+      subject:"contact",
+      html:`<p>${message}</p>`
+    })
+    console.log('message',emailTransport.messageId)
+  } catch (error) {
+    console.log(error)
+  }
+  res.status(200).json({ name: name })
+}

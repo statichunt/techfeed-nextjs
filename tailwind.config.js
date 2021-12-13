de4076acd0789d07d669e276c7fa02c91776ef19
omar-faruk02 <tuhin.omar99@gmail.com>
2021-12-13 16:16:18 +0600

    Social icon problem fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  content:["./pages/**/*.{js,ts,jsx,tsx}",
-  "./component/**/*.{js,ts,jsx,tsx}",
+  content: ["./pages/**/*.{js,ts,jsx,tsx}",
+    "./component/**/*.{js,ts,jsx,tsx}",
   ],
 
   darkMode: 'media',
-  
+
   variants: {
     extend: {
       variantOrder: [
@@ -30,63 +30,63 @@ module.exports = {
   plugins: [],
 
   theme: {
-  extend:{
-     colors:{
-       buttonColor:"#c8ab77"
-     },
-    fontSize: {
-      'xs': '.75rem',
-      'sm': '.875rem',
-      'tiny': '.875rem',
-       'base': '1rem',
-       'lg': '1.125rem',
-       'xl': '1.25rem',
-       '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-       '5xl': '3rem',
-       '6xl': '4rem',
-      '7xl': '5rem',
-     },
-     fontFamily:{
-       'display':['"Oswald"','sans-serif'],
-       'lora':['"Lora"','serif']
-     },
-     textColor:{
-       'headingColor':'#333333',
-       'commonColor':'#c8ab77',
-       'commonColorHover':'#9b7a3e',
-       'nameColor':'#aaa',
-       'linkHoverColor':'#f7f8fa',
-       'white':'#ffff',
-       'black':'#000',
-       'green':'green',
-       'red':'red'
-       
-     },
-     borderColor:{
-       'commonColor':'#c8ab77',
-       'bannerIconBorder':'#ddd',
-       'footerBorder':'#dee2e6'
-     },
-     height: {
-       h1:"0.25rem",
-       bannerHeight:"600px",
-       h12:"3rem",
-       h16:"4rem",
-       h32:"8rem",
-       logoHeight:"160px",
-       authorImage:"13rem",
-       h200:"200px",
-       h400:"400px"
+    extend: {
+      colors: {
+        buttonColor: "#c8ab77"
+      },
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+      },
+      fontFamily: {
+        'display': ['"Oswald"', 'sans-serif'],
+        'lora': ['"Lora"', 'serif']
+      },
+      textColor: {
+        'headingColor': '#333333',
+        'commonColor': '#c8ab77',
+        'commonColorHover': '#9b7a3e',
+        'nameColor': '#aaa',
+        'linkHoverColor': '#f7f8fa',
+        'white': '#ffff',
+        'black': '#000',
+        'green': 'green',
+        'red': 'red'
+
+      },
+      borderColor: {
+        'commonColor': '#c8ab77',
+        'bannerIconBorder': '#ddd',
+        'footerBorder': '#dee2e6'
+      },
+      height: {
+        h1: "0.25rem",
+        bannerHeight: "600px",
+        h12: "3rem",
+        h16: "4rem",
+        h32: "8rem",
+        logoHeight: "160px",
+        authorImage: "13rem",
+        h200: "200px",
+        h400: "400px"
 
 
       },
 
-  }
-     
-   
-   
+    }
+
+
+
   }
 
 }

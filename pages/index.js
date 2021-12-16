4e255eca19a9c02c49735b7c61743e4e83d806fb
omar-faruk02 <tuhin.omar99@gmail.com>
2021-12-16 23:53:29 +0600

    fixed 500 error

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import { AppContext } from '../component/AppContext';
 const currentDate=new Date()
 const Home = ({ posts, page,aboutFrontMatter }) => {
   const [postLength,setPostLength]=useContext(AppContext)
-  console.log(aboutFrontMatter)
+  
   
   useEffect(()=>{
     setPostLength(posts.length)
@@ -51,12 +51,7 @@ export const getServerSideProps = async ({ query: { page = 1 } }) => {
 
   // about page data
 
-  const aboutFile = fs.readdirSync(path.join("About"));
-  const metaDataWithFrontMatter = fs.readFileSync(
-    path.join("About", aboutFile[0]),
-    "utf-8"
-  );
-  const { data: aboutFrontMatter } = matter(metaDataWithFrontMatter);
+  
   
   
   
@@ -65,7 +60,7 @@ export const getServerSideProps = async ({ query: { page = 1 } }) => {
     props: {
       posts:filterByDate,
       page: +page,
-      aboutFrontMatter:aboutFrontMatter,
+      
       
     }
 

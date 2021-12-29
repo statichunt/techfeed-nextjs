c101a49a226f8baa726ef7a831e3ed9a0a590b8a
omar-faruk02 <tuhin.omar99@gmail.com>
2021-12-29 13:48:52 +0600

    commit for solve

--- component/About/Author.js
+++ component/About/Author.js
@@ -1,29 +1,29 @@
-// import React from "react";
-// import Image from "next//image";
+import React from "react";
+import Image from "next//image";
 
 
-// export default function Author({aboutData}) {
-//   console.log(aboutData)
-//   return (
-//     <div className="mt-24 PostContainer">
-//       <div className="flex justify-center  items-center flex-col mx-auto lg:max-w-full">
-//         <div className="welcomeContent">
-//           <Image
-//             className="welcomeImage"
-//             alt=""
-//             src={aboutData.frontmatter.aboutImage}
-//             layout="fill"
-//             objectFit="cover"
-//           ></Image>
-//         </div>
+export default function Author({aboutData}) {
+  console.log(aboutData)
+  return (
+    <div className="mt-24 PostContainer">
+      <div className="flex justify-center  items-center flex-col mx-auto lg:max-w-full">
+        <div className="welcomeContent">
+          <Image
+            className="welcomeImage"
+            alt=""
+            src={aboutData.frontmatter.aboutImage}
+            layout="fill"
+            objectFit="cover"
+          ></Image>
+        </div>
 
-//         <div className="heading sm:text-30px font-medium leading-5 my-12 ">
-//           <h3>{aboutData.frontmatter.greetings}</h3>
-//         </div>
-//         <div className="authorDetails">
-//           <p className="">{aboutData.frontmatter.about}</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+        <div className="heading sm:text-30px font-medium leading-5 my-12 ">
+          <h3>{aboutData.frontmatter.greetings}</h3>
+        </div>
+        <div className="authorDetails">
+          <p className="">{aboutData.frontmatter.about}</p>
+        </div>
+      </div>
+    </div>
+  );
+}

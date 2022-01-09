b8c645dc971231ffeba1303ea2fa121b1598475d
omar-faruk02 <tuhin.omar99@gmail.com>
2022-01-09 17:12:07 +0600

    add 404 in md

--- pages/404.js
+++ pages/404.js
@@ -1,19 +1,30 @@
 import React from "react";
 import NotFound from "config/404.json";
 import Layout from "@/component/Layout";
+import { get404Page } from "@/lib/post";
 
-function Error() {
-  const { title } = NotFound;
+function Error(notFoundData) {
+  console.log(notFoundData);
   return (
-    <Layout title={title}>
+    <Layout>
       <div className="container text-center font-primary mx-auto">
         <div className="py-32">
-          <h1 className="text-h1">404</h1>
-          <h4 className="text-h4 ">Page Not Found</h4>
+          {/* <h1 className="text-h1">{notFoundData.frontmatter.title}</h1>
+          <h4 className="text-h4 ">{notFoundData.frontmatter.subTitle}</h4> */}
         </div>
       </div>
     </Layout>
   );
 }
+
+export const getStaticProps = () => {
+  const notFoundData = get404Page();
+  console.log(notFoundData);
+  return {
+    props: {
+      notFoundData,
+    },
+  };
+};
 
 export default Error;
